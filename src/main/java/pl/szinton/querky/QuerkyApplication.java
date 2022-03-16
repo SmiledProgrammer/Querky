72f7f7a53faaf5224882db5868cf7f72fe7f088e
szinton <szinton@gmail.com>
2022-03-16 10:22:05 +0100

    Initial commit

---
+++ src/main/java/pl/szinton/querky/QuerkyApplication.java
@@ -0,0 +1,13 @@
+package pl.szinton.querky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/szinton/querky/QuerkyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szinton.querky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
