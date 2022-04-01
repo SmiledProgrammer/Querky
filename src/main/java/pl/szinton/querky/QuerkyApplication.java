385b4396648ab95bb1ecbd8f97024b3242c8ac8d
szinton <szinton@gmail.com>
2022-04-01 17:25:22 +0200

    Too late db start fix

--- src/main/java/pl/szinton/querky/QuerkyApplication.java
+++ src/main/java/pl/szinton/querky/QuerkyApplication.java
@@ -2,12 +2,22 @@ package pl.szinton.querky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class QuerkyApplication {
+
+    @GetMapping("/email")
+    public String getEmail() {
+        DefaultOAuth2User user = (DefaultOAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return String.valueOf(user.getAttributes().get("email"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(QuerkyApplication.class, args);
     }
-
 }
