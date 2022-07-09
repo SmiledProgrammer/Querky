890ba995b11624981c65c9fddf05b6eab22f6908
szinton <szinton@gmail.com>
2022-07-09 17:09:27 +0200

    RabbitMQ + WebSockets setup for tests

---
+++ src/main/java/pl/szinton/querky/config/WebSocketTestConfiguration.java
@@ -0,0 +1,25 @@
+package pl.szinton.querky.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@Profile("test")
+public class WebSocketTestConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry
+                .setApplicationDestinationPrefixes("/app")
+                .enableSimpleBroker();
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+}
